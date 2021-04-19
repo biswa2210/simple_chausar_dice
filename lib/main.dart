010ed35e5f45aa599a5ec0106c334c715f7fa3d5
Biswarup Bhattacharjee <bbiswa471@gmail.com>
2021-04-19 12:19:47 +0530

    ADDED BY BISWA

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        title: Center(child:Text("Chaucer Dieces",style: TextStyle(fontFamily: 'SR',letterSpacing: 5,fontSize: 30),)),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: DieceePage(),
+    ),
+  ));
+}
+class DieceePage extends StatefulWidget {
+  @override
+  _DieceePageState createState() => _DieceePageState();
+}
+
+class _DieceePageState extends State<DieceePage> {
+  @override
+  int leftDieceNumber=1;
+  int RightDieceNumber=2;
+  void changedieceeface(){
+    RightDieceNumber=Random().nextInt(6)+1;
+    leftDieceNumber=Random().nextInt(6)+1;
+  }
+  Widget build(BuildContext context) {
+    return Center(
+        child:Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Expanded(
+              child:Padding(
+                padding:const EdgeInsets.all(16.0),
+                child: FlatButton(
+                    onPressed: (){
+                      setState(() {
+                        changedieceeface();
+                      });
+                    },
+                    child:Image.asset('assets/images/dice${leftDieceNumber}.png')),
+              ),),
+            Expanded(
+              child:Padding(
+                padding: const EdgeInsets.all(16.0),
+                child:FlatButton(
+                    onPressed: (){
+                     setState(() {
+                     changedieceeface();
+                     });
+                    },
+                    child:Image.asset('assets/images/dice${RightDieceNumber}.png')
+                ),
+              ),),
+          ],
+        )
+    );
+  }
+}
+
